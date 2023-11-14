89555a6ac49ce279850cf059331b46c79f45753d
Diego Dias <emerson.diego.duarte@gmail.com>
2023-11-14 16:07:29 -0300

    first commit

---
+++ src/test/java/com/example/techchallengecliente/TechChallengeClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techchallengecliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
